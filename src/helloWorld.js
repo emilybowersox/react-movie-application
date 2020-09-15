eb7d9ec73efe3abff700746edb7c8d3895b6e6d9
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-15 14:17:35 -0500

    working with react

---
+++ src/helloWorld.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './helloWorld.css';
+
+function helloWorld(){
+    return (
+        <div className="body">
+            <header>
+                <p>
+                    Hello world, it's <code>me!</code>
+                </p>
+            </header>
+        </div>
+    );
+}
+
+// ? what does this do?
+export default helloWorld;
+
