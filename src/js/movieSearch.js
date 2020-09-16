f70e3fa3c5508d8deafbd60b0c109180a455fa8e
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-16 14:17:52 -0500

    apikey file in gitignore

--- src/js/movieSearch.js
+++ src/js/movieSearch.js
@@ -2,14 +2,23 @@ import React from "react";
 import '../css/style.css';
 
 export default function MovieSearch() {
+
+    const movieSearch = async (e) => {
+        e.preventDefault()
+        console.log("submitting");
+    }
+
+    //hardcoding a value since we're not pulling it from the input form yet
+    const query = "Booksmart";
+
+    //es6 template literals- the backticks ` ` to reference the value of the variable in ${}
+    const apiUrl = `"https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=${query}&page=1&include_adult=false"``;
+
     return (
-        <form className="form" action="">
+        <form className="form" onSubmit={movieSearch}>
             <label className="label" htmlFor="query">Movie Name:</label>
             <input className="input" type="text" name="query" placeholder="i.e. Booksmart"/>
             <button className="button" type="submit">Search</button>
         </form>
-
-        // <h1>Hello World</h1>
     )
-
 }
