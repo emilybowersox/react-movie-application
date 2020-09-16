a6d05732ba529a4b839ff608bd30617568097eaf
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-16 14:35:50 -0500

    asynch await for grabbing api data

--- src/js/movieSearch.js
+++ src/js/movieSearch.js
@@ -6,13 +6,19 @@ export default function MovieSearch() {
     const movieSearch = async (e) => {
         e.preventDefault()
         console.log("submitting");
+
+        //hardcoding a value since we're not pulling it from the input form yet
+        const query = "Booksmart";
+
+        //es6 template literals- the backticks ` ` to reference the value of the variable in ${}
+        const url = `"https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=${query}&page=1&include_adult=false"`;
+
+        //asynch await
+        const res = await fetch(url);
+        //converting the raw response to data
+        const data = await res.json();
+        console.log(data);
     }
-
-    //hardcoding a value since we're not pulling it from the input form yet
-    const query = "Booksmart";
-
-    //es6 template literals- the backticks ` ` to reference the value of the variable in ${}
-    const apiUrl = `"https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=${query}&page=1&include_adult=false"``;
 
     return (
         <form className="form" onSubmit={movieSearch}>
